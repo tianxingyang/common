8e82e1ce021902b7f253ca767abb38a6e97a71f7
杨天幸 <versacrop@outlook.com>
2018-07-17 11:09:51 +0800

    修改测试

--- test/vlist_test.cpp
+++ test/vlist_test.cpp
@@ -14,7 +14,7 @@ TEST(VListTest, ConstructorTest)
     EXPECT_EQ(v_list_1.head_->next_->data_, 10);
     EXPECT_EQ(v_list.head_->next_, nullptr);
     EXPECT_EQ(v_list_1.tail_->next_->data_, 10);
-    EXPECT_EQ(v_list_1.tail_->next_, nullptr);
+    EXPECT_EQ(v_list_1.tail_->next_, v_list_1.head_->next_);
 }
 
 TEST(VListTest, IteratorTest)
