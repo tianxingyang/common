782ae671b3d4065bee8ea7b09efe351ed21f007f
vitoyang <mr.versac@gmail.com>
2018-07-18 15:15:50 +0800

    =修改测试

--- tests/vlist_test.cpp
+++ tests/vlist_test.cpp
@@ -7,22 +7,22 @@ using namespace vcommon;
 
 INITIALIZE_EASYLOGGINGPP
 
-//TEST(VListTest, ConstructorTest)
-//{
-//    VList<int> v_list;
-//    VList<int> v_list_1(10);
-//    EXPECT_EQ(v_list_1.head_->next_->data_, 10);
-//    EXPECT_EQ(v_list.head_->next_, nullptr);
-//    EXPECT_EQ(v_list_1.tail_->next_->data_, 10);
-//    EXPECT_EQ(v_list_1.tail_->next_, v_list_1.head_->next_);
-//}
-//
-//TEST(VListTest, IteratorTest)
-//{
-//    VList<int> v_list(10);
-//    EXPECT_EQ(*(v_list.begin()), 10);
-//    EXPECT_EQ(*(v_list.end()), 10);
-//}
+TEST(VListTest, ConstructorTest)
+{
+    VList<int> v_list;
+    VList<int> v_list_1(10);
+    EXPECT_EQ(v_list_1.head_->next_->data_, 10);
+    EXPECT_EQ(v_list.head_->next_, nullptr);
+    EXPECT_EQ(v_list_1.tail_->next_->data_, 10);
+    EXPECT_EQ(v_list_1.tail_->next_, v_list_1.head_->next_);
+}
+
+TEST(VListTest, IteratorTest)
+{
+    VList<int> v_list(10);
+    EXPECT_EQ(*(v_list.begin()), 10);
+    EXPECT_EQ(*(v_list.end()), 10);
+}
 
 int main(int argc, char* argv[])
 {
